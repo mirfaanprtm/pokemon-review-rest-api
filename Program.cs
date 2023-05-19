915021e161d90e0bb670897e56e3945181448118
Irfan <irfanpratama264@gmail.com>
2023-05-19 19:31:37 +0700

    asp .net core rest api

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ using WebApplication2.Repository;
 
 var builder = WebApplication.CreateBuilder(args);
 
+
 // Add services to the container.
 builder.Services.AddControllers();
 builder.Services.AddTransient<Seed>();
